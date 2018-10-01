2a04a39987e50a9ee56b48421999e90fdeeea398
Luciano Strika <lucianostrika44@gmail.com>
2018-10-01 19:45:45 -0300

    add implementation of new function

--- main.asm
+++ main.asm
@@ -65,3 +65,76 @@ average_byte:
     pop rbx
     pop rbp
     ret
+
+global replace_byte
+replace_byte:
+    ;stack frame
+    push rbp
+    push rbx
+    push r15
+    
+    ; rdi : pointer to stream
+    ; rsi : have
+    ; rdx : want
+    xor rax, rax
+    pxor xmm1, xmm1
+    .get_length:
+    movdqu xmm0, [rdi + rax]
+    pcmpeqb xmm0, xmm1
+    vpmovmskb rbx, xmm0
+
+    add rax, 16
+
+    cmp rbx, 0
+    jne .border_case
+    jmp .get_length
+    
+    .border_case:
+    popcnt rbx, rbx
+    sub rax, rbx ; now rax has the length of the string
+
+    .replace:
+    xor rbx, rbx ; will be used for indexing
+    pxor xmm3, xmm3
+
+    movd xmm1, esi ; xmm1 has 'have' on lower word
+    movd xmm2, edx ; xmm2 has 'want' on lower word
+
+    pshufb xmm2, xmm3 ;repeat least significant byte 16 times
+    pshufb xmm1, xmm3 ;repeat least significant byte 16 times
+
+    .cycle_read:
+    cmp rbx, rax 
+    jge .end_cycle
+    movdqu xmm0, [rdi + rbx]
+    movdqa xmm3, xmm0
+    pcmpeqb xmm3, xmm1 ; check where avoided byte is
+    
+    pcmpeqb xmm4, xmm4 ; turn everything on
+    pxor xmm4, xmm3 ; turn on where avoided byte ISN'T
+
+    pand xmm3, xmm2 ; turn on wanted byte where avoided was
+    pand xmm0, xmm4 ; restrict stream to wanted bytes
+    por xmm0, xmm3 ; turn on replacement
+    movdqu [rdi + rbx], xmm0
+    add rbx, 16
+
+    jmp .cycle_read
+    .end_cycle:
+    
+    ; restore stack
+    pop r15
+    pop rbx
+    pop rbp
+    ret
+
+
+
+
+
+
+
+
+
+
+
