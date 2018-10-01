e13e72c697957027eb6994eb01403bff050c1f28
Luciano Strika <lucianostrika44@gmail.com>
2018-10-01 19:45:29 -0300

    add new function

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-extern double average_byte(char* s); 
+extern double average_byte(char* s);
+extern void replace_byte(char* s, char have, char want);
+ 
 int main(){
     printf("Starting program...\n");
     char* s = malloc(32);
@@ -11,5 +13,15 @@ int main(){
     s[31] = 0;
     double r = average_byte(s);
     printf("%.3f ",r);
+
+    for(int i = 0; i < 32; i++){
+        printf("%d ",s[i]);
+    }
+    printf("\n THEN \n");
+    replace_byte(s, 1, 18);
+    for(int i = 0; i < 32; i++){
+        printf("%d ",s[i]);
+    }
+    printf("\n");
     return 0;
 }
