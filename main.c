143e3beee3df4d00aff719d017b01a4f7d1f3979
Luciano Strika <lucianostrika44@gmail.com>
2018-10-05 20:12:17 -0300

    add C implementation and remove unnecessary printfs

--- main.c
+++ main.c
@@ -1,27 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 extern double average_byte(char* s);
 extern void replace_byte(char* s, char have, char want);
- 
+void replace_byte_c(char* s, char have, char want){
+    int i = 0;
+    while(s[i]!=0){
+        if(s[i]==have){
+	    s[i]=want;
+	}
+	i++;
+    }
+}
+void show_stream(char* s, int length){
+    for(int i = 0; i < length; i++){
+        printf("%d ",s[i]);
+    }
+    printf("\n");
+} 
 int main(){
+    int SIZE = 1600000000;
     printf("Starting program...\n");
-    char* s = malloc(32);
-    for(int i = 0; i < 31; i++){
+    char* s = malloc(SIZE);
+
+    for(int i = 0; i < SIZE-1; i++){
         s[i] = (i%3)+1;	
     }
-    s[31] = 0;
-    double r = average_byte(s);
-    printf("%.3f ",r);
+    s[SIZE-1] = 0;
+    printf("Initialized array\n");
 
-    for(int i = 0; i < 32; i++){
-        printf("%d ",s[i]);
-    }
-    printf("\n THEN \n");
+    //show_stream(s, SIZE);
+    float startTime = (float)clock()/CLOCKS_PER_SEC;
     replace_byte(s, 1, 18);
-    for(int i = 0; i < 32; i++){
-        printf("%d ",s[i]);
-    }
+    float endTime = (float)clock()/CLOCKS_PER_SEC;
+    float time_asm = endTime - startTime;
+    //show_stream(s,SIZE);
+    printf("Finished ASM.\n");
+    startTime = (float)clock()/CLOCKS_PER_SEC;
+    replace_byte_c(s, 1, 18);
+    endTime = (float)clock()/CLOCKS_PER_SEC;
+    printf("Finished C.\n");
+    float time_c = endTime - startTime;
+
+   // show_stream(s,SIZE);
+    printf("%.3f vs %.3f", time_asm, time_c);
     printf("\n");
     return 0;
 }
